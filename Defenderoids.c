fc8305554753bc78f8fc964667e55a66d3e88de6
ahchay <chrisahchay@gmail.com>
2019-03-22 10:14:17 +0000

    Experimenting with rotating bitmaps rather than line vectors.

--- Defenderoids.c
+++ Defenderoids.c
@@ -64,8 +64,10 @@ void DefenderoidsMain()
 
 		CreateBitmap((u16*)RugBitmap, 128, 128);
 
-		iStartX=DefenderoidShip[0][0];
-		iStartY=DefenderoidShip[0][1];
+		// Line version...
+		/*
+		iStartX=DefenderoidShip[0].x;
+		iStartY=DefenderoidShip[0].y;
 		iPoint = 0;
 
 		while (iPoint++<4)
@@ -75,48 +77,73 @@ void DefenderoidsMain()
 			//PrintDecimal(SCR_1_PLANE, 0, 12, iPoint + 12, iStartY, 4);
 
 			// Start with a border
-			DrawLine((u16*)RugBitmap,(u8)((iStartX)*63),(u8)((iStartY)*63),(u8)((DefenderoidShip[iPoint][0])*63),(u8)((DefenderoidShip[iPoint][1])*63),1);
+			DrawLine((u16*)RugBitmap,(u8)((iStartX)*127),(u8)((iStartY)*127),(u8)((DefenderoidShip[iPoint].x)*127),(u8)((DefenderoidShip[iPoint].y)*127),1);
 
-			// Can we scale this easily?
-			DrawLine((u16*)RugBitmap,(u8)(1+(iStartX)*iScale),(u8)(1+(iStartY)*iScale),(u8)(1+(DefenderoidShip[iPoint][0])*iScale),(u8)(1+(DefenderoidShip[iPoint][1])*iScale),2);
+			// Scale is relatively easy
+			DrawLine((u16*)RugBitmap,(u8)(1+(iStartX)*iScale),(u8)(1+(iStartY)*iScale),(u8)(1+(DefenderoidShip[iPoint].x)*iScale),(u8)(1+(DefenderoidShip[iPoint].y)*iScale),2);
 
-			// Or rotate? Using integer maths?
+			// Rotation needs a bit more calculation
 			// Library C functions return a signed integer range, not a "true" sin/cos
 			cSin = Sin(iAngle);
 			cCos = Cos(iAngle);
 
-			// translate point back to origin:
-			iStartX = (iStartX<<3)-8;
-			iStartY = (iStartY<<3)-8;
+			// translate point to an arbitary origin:
+			// This acts as the centre of rotation, scaling done here too.
+			iStartX = (iStartX<<3)-16;
+			iStartY = (iStartY<<3)-16;
 
 			// rotate point.
-			// The calculation here is xpos = (xpos * cos(theta)) - (ypos * sin(theta))
+			// The calculation here should be xpos = (xpos * cos(theta)) - (ypos * sin(theta))
 			// Because our sin/cos table is an integer table and not the "true" sin/cos
 			// we need to do each side of that calculation and then divide down by 128 (or right shift 7 points)
 			// before adding/subtracting the two results
 			iTempX = ((iStartX * cCos)>>7) - ((iStartY * cSin)>>7);
 			iTempY = ((iStartX * cSin)>>7) + ((iStartY * cCos)>>7);
 
-			// translate point back:
-			iStartX = iTempX + 8;
-			iStartY = iTempY + 8;
+			// translate point back to it's original position:
+			iStartX = iTempX + 16;
+			iStartY = iTempY + 16;
 
 			// translate point back to origin:
-			iEndX = (DefenderoidShip[iPoint][0]<<3)-8;
-			iEndY = (DefenderoidShip[iPoint][1]<<3)-8;
+			iEndX = (DefenderoidShip[iPoint].x<<3)-16;
+			iEndY = (DefenderoidShip[iPoint].y<<3)-16;
 
 			// rotate point
 			iTempX = ((iEndX * cCos)>>7) - ((iEndY * cSin)>>7);
 			iTempY = ((iEndX * cSin)>>7) + ((iEndY * cCos)>>7);
 
 			// translate point back:
-			iEndX = iTempX + 8;
-			iEndY = iTempY + 8;
+			iEndX = iTempX + 16;
+			iEndY = iTempY + 16;
 
 			DrawLine((u16*)RugBitmap,(u8)(1+((126-16)>>1)+(iStartX)),(u8)(1+((126-16)>>1)+(iStartY)),(u8)(1+((126-16)>>1)+(iEndX)),(u8)(1+((126-16)>>1)+(iEndY)),3);
 
-			iStartX = DefenderoidShip[iPoint][0];
-			iStartY = DefenderoidShip[iPoint][1];
+			iStartX = DefenderoidShip[iPoint].x;
+			iStartY = DefenderoidShip[iPoint].y;
+
+		}
+		*/
+
+		// Point version
+		iPoint = 0;
+		while (iPoint++<11)
+		{
+
+			cSin = Sin(iAngle);
+			cCos = Cos(iAngle);
+
+			iStartX = (PrettyPattern[iPoint].x<<1)-8;
+			iStartY = (PrettyPattern[iPoint].y<<1)-8;
+
+			// rotate point.
+			iTempX = ((iStartX * cCos)>>7) - ((iStartY * cSin)>>7);
+			iTempY = ((iStartX * cSin)>>7) + ((iStartY * cCos)>>7);
+
+			// translate point back to it's original position:
+			iStartX = iTempX + 8;
+			iStartY = iTempY + 8;
+
+			SetPixel((u16*)RugBitmap,(u8)(1+((126-16)>>1)+(iStartX)),(u8)(1+((126-16)>>1)+(iStartY)),PrettyPattern[iPoint].colour);
 
 		}
 
@@ -148,6 +175,8 @@ void DefenderoidsMain()
 		// Interestingly, creating with only a static box only takes it down to 4 or five blanks per frame?
 		// Implying that maybe it's the "create and copy bitmap" that's taking a significant amount of that time.
 		// We know that PutPixel is a bit slow as well, so the longer the line the longer it will take...
+		// 1 or 2 frames if we use individual pixel maps?
+		// Could I do "proper" rotatable sprites like this?
 
 		//DrawBitmap((u16 *)bmpBackground, 160, 96, 0, 2);
 	}
--- Defenderoids.h
+++ Defenderoids.h
@@ -1,5 +1,20 @@
 #define bgTileBase 256
 
+typedef struct Point
+{
+	u16 x;
+	u16 y;
+} POINT;
+
+typedef struct ColourPoint
+{
+	u16 x;
+	u16 y;
+	u8 colour;
+} COLOURPOINT;
+
 void DefenderoidsMain();
 
-const u16 DefenderoidShip[5][2] = {{0,0},{0,2},{2,2},{2,0},{0,0}};
+const POINT DefenderoidShip[5] = {{0,0},{0,1},{1,1},{1,0},{0,0}};
+
+const COLOURPOINT PrettyPattern[12] = {{0,0,1},{3,3,1},{5,5,1},{7,7,1},{12,12,3},{10,10,3},{8,8,3},{6,6,3},{0,8,2},{2,6,2},{4,4,2},{6,2,2}};
